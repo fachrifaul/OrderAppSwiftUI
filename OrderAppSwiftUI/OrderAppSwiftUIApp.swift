6b6ad849e910c4efac24c558f2ec844c52514a70
Fachri Febrian <fachripaul@gmail.com>
2025-04-10 12:55:23 +0700

    Initial Commit

---
+++ OrderAppSwiftUI/OrderAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderAppSwiftUIApp.swift
+//  OrderAppSwiftUI
+//
+//  Created by Fachri Febrian on 10/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct OrderAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
